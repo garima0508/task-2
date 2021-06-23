e51e8d6332aaa150948cf4ca115a394c238c104f
Garima <gammo0508@gmail.com>
2021-06-23 21:10:48 +0530

    dummy

---
+++ app.js
@@ -0,0 +1,24 @@
+TweenMax.defaultEase = Linear.easeOut;
+
+new fullpage("#fullpage", {
+  autoScrolling: true,
+  navigation: true,
+  onLeave: (origin, destination, direction) => {
+    const section = destination.item;
+    const title = section.querySelector("h1");
+    const tl = new TimelineMax({ delay: 0.5 });
+    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
+    if (destination.index === 1) {
+      const form = document.querySelectorAll(".form");
+      const description = document.querySelector(".description");
+      tl.fromTo(form, 0.7, { x: "100%" }, { x: "-10%" })
+        .fromTo(
+          description,
+          0.5,
+          { opacity: 0, y: "50" },
+          { y: "0", opacity: 1 }
+        )
+        .fromTo(form, 1, { opacity: 1 }, { opacity: 1 });
+    }
+  },
+});
